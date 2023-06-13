84ecb000db82b87238a980b752264131ac97d9e0
teratti <c0b22106d3@edu.teu.ac.jp>
2023-06-13 15:24:27 +0900

    問２

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    ton_img=pg.image.load("ex01/fig/3.png")
+    ton_img=pg.transform.flip(ton_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
