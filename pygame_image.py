d01f32ae33db686f3d2e841ba1408519d9075bca
teratti <c0b22106d3@edu.teu.ac.jp>
2023-06-13 16:34:05 +0900

    問５

--- pygame_image.py
+++ pygame_image.py
@@ -10,24 +10,26 @@ def main():
     ton_img=pg.image.load("ex01/fig/3.png")
     ton_img=pg.transform.flip(ton_img,True,False)
     ton_img2=pg.transform.rotozoom(ton_img,10,1.0)
+    angle=0
+
     ton_lst=[ton_img , ton_img2]
     
 
     tmr = 0
+    angle_plus=0.1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        ton_img3=pg.transform.rotozoom(ton_img,angle,1.0)
 
         screen.blit(bg_img, [-(tmr%1600), 0])
-        
-
         i=tmr%2
-
-
         screen.blit(ton_lst[i],[300,200])
-
         pg.display.update()
-        tmr += 1        
+        tmr += 1  
+        angle+=angle_plus
+        if angle>10 or angle<=0:  
+            angle_plus*=-1
         clock.tick(100)
 
 
