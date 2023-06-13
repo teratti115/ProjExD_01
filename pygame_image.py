91d0c7c8a5d3016fd79338314564532e58be187e
teratti <c0b22106d3@edu.teu.ac.jp>
2023-06-13 17:07:24 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -11,6 +11,8 @@ def main():
     ton_img=pg.transform.flip(ton_img,True,False)
     ton_img2=pg.transform.rotozoom(ton_img,10,1.0)
     angle=0
+    bg_img_rev=pg.transform.flip(bg_img,True,False)
+
 
     ton_lst=[ton_img , ton_img2]
     
@@ -21,18 +23,23 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         ton_img3=pg.transform.rotozoom(ton_img,angle,1.0)
-        yoko=-(tmr%1600)
-        screen.blit(bg_img, [yoko, 0])
+        screen.blit(bg_img, [-yoko, 0])
+        screen.blit(bg_img_rev, [1600-yoko, 0])
+        screen.blit(bg_img, [3200-yoko, 0])
+
         i=tmr%2
         screen.blit(ton_img3,[300,200])
         pg.display.update()
         tmr += 1  
         angle+=angle_plus
+        yoko+=1
         if angle>10 or angle<=0:  
             angle_plus*=-1
-        clock.tick(100)
-        if yoko<-1599:
-            bg_img=pg.transform.flip(bg_img,True,False)
+        clock.tick(800)
+        if yoko>=3200:
+            yoko=0
+
+            
 
 
 
